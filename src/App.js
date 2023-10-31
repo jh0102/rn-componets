bb464bcea318c7cf12ca638ac2047916842d66ef
jh0102 <halo0102@naver.com>
2023-10-31 18:15:26 +0900

    리액네 컴포넌트 연습

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import MyButton from './MyButton';
+
+export default function App() {
+  const [addition, setAddition] = useState(0);
+  const [multiple, setMultiple] = useState(1);
+  return (
+    <View style={styles.container}>
+      <TextInput 
+      onChange={text => console.log(text)}
+      style={{borderWidth: 1, padding: 10, fontSize:20}}
+      />
+       {/* <Text style={{fontSize:20}}>{addition}</Text> 
+        <Text style={{fontSize:20}}>{multiple}</Text> 
+      <MyButton title="addition" onPress={() => setAddition(addition + 2)}/>
+  <MyButton title="multiple" onPress={() => setMultiple(multiple * 2)}/> */}
+          </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
